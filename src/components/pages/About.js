40bee76bc2fc22afb259275c1836baf5c76edb7e
Warisha <warisharehman75@gmail.com>
2020-11-04 12:52:09 +0500

    all files commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the Todo List app v1.0.0. It is part of a react crash course. </p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
